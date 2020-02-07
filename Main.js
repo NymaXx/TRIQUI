dad25a2cc64392e72ab872b753ef6b561207f494
Miguel Angel Rivera <angel1301999@gmail.com>
2020-02-07 15:05:37 -0500

    implementacion cuadriculas

--- Main.js
+++ Main.js
@@ -1,8 +1,12 @@
+
 function setup(){
-    createCanvas(600,600);
+    createCanvas(500,500);
 }
 
 function draw(){
-ellipse(50,50,05,50);
+rect(200,100,205,500);
+rect(300,100,305,500);
+rect(100,200,500,205);
+rect(100,300,500,305);
 }
 
