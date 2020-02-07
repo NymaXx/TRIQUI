fe71b22637708b258150c7f2b8c9d31dc380b54e
[NymaXxX] <[dulze8246@gmail.com]>
2020-02-07 15:12:06 -0500

    clase figure

---
+++ Figure.js
@@ -0,0 +1,17 @@
+class Figure{
+    constructor(tamX, tamY, posX, posY, turn){
+        this.tamX= tamX;
+        this.tamY= tamY;
+        this.posX= posX;
+        this.posY= posY;
+        this.turn= turn;
+    }
+
+    pintar(){
+        
+    }
+
+    
+
+
+}
