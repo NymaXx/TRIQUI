94867b4f20e60e1773a024f97481a581415abc65
Jhon Garcia <metalprime57@gmail.com>
2020-02-13 10:43:19 -0500

    cambios nombres pt.2

--- Main.js
+++ Main.js
@@ -1,4 +1,5 @@
 let posX, posY;
+let color;
 let matrizTriqui = [
     [0, 0, 0],
     [0, 0, 0],
@@ -10,10 +11,11 @@ function setup() {
     createCanvas(500, 500);
     posX = 100;
     posY = 100;
+    color = 255;
     for (let i = 0; i < matrizTriqui.length; i++) {
         for (let j = 0; j < matrizTriqui.length; j++) {
 
-            fill(255, 255, 255);
+            fill(color);
             rect(posX * j, posY * i, 100, 100);
 
         }
@@ -25,7 +27,9 @@ function draw() {
     for (let i = 0; i < matrizTriqui.length; i++) {
         for (let j = 0; j < matrizTriqui.length; j++) {
 
-
+            
+            fill(color);
+            rect(posX * j, posY * i, 100, 100);
             strokeWeight(4);
             stroke(51);
             
@@ -37,7 +41,21 @@ function draw() {
 }
 
 function mousePressed() {
-    if (XM) {
+    for (let i = 0; i < matrizTriqui.length; i++) {
+        for (let j = 0; j < matrizTriqui.length; j++) {
+            console.log(color);
+            if(matrizTriqui[j][i]==1){
+                color=0;
+            } else {
+                color=255;
+            }
 
-    };
+            if(matrizTriqui[j][i]==0 ){
+                matrizTriqui[j][i]=1;
+            }
+            
+            
+        }
+}
+
 }
